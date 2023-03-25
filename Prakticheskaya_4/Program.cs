b2169c9686c0abd923cdadc204cb76a54730e131
sacha <sacha@DESKTOP-K3ISJ8P>
2023-03-25 23:41:50 +0300

    Добавьте файлы проекта.

---
+++ Prakticheskaya_4/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+
+namespace Prakticheskaya_4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<IHello> myHello = new List<IHello>();
+
+            myHello.Add(new Russian());
+            myHello.Add(new English());
+            myHello.Add(new Spanish());
+
+            foreach (IHello hello in myHello)
+            {
+                hello.SayHello();
+            }
+
+            Console.ReadKey(true);
+        }
+    }
+    interface IHello
+    {
+        void SayHello();
+    }
+
+    class Russian : IHello
+    {
+        public void SayHello()
+        {
+            Console.WriteLine("Привет");
+        }
+    }
+    class English : IHello
+    {
+        public void SayHello()
+        {
+            Console.WriteLine("Hello");
+        }
+    }
+    class Spanish : IHello
+    {
+        public void SayHello()
+        {
+            Console.WriteLine("Hola");
+        }
+    }
+}
